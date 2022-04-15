16d3a244c4ff7623aa4e72a819531786fe47742e
sangheekim <tkdgmltjddnr@naver.com>
2022-04-15 11:33:41 +0900

    js 수정 작업

---
+++ js/youtube.js
@@ -0,0 +1,4 @@
+const api_key = 'AIzaSyBXcN2dhap7pG4dBHHa6NV25JXQB98JpZY';
+const num = 5;
+const vidid = 'PLyLpzOyPZPDySVKOdpOZPfo4IkSVp-4Gu';
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${api_key}&maxResults=${num}&playlistId=${vidid}`;
